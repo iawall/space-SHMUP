9ec4e167da24f807fe7d896fbb807c507bbaa4e7
iawall <iawall@siue.edu>
2025-03-30 19:14:40 -0500

    Initial commit

---
+++ Assets/__Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+ using System.Collections.Generic;
+ using UnityEngine;
+ public class Projectile : MonoBehaviour {
+    private BoundsCheck     bndCheck;
+    void Awake () {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+    void Update () {
+        if (bndCheck.offUp) {                                // a
+            Destroy( gameObject );
+        }
+    }
+ }
